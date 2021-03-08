b8b3d70c8d47f16ef701bf3a687400554fca6fd8
xiong35 <2242787668@qq.com>
2021-03-08 16:28:17 +0800

    change lexical to English

--- src/lexicalAnalyse.cpp
+++ src/lexicalAnalyse.cpp
@@ -1,178 +1,177 @@
 #include "lexicalAnalyse.h"
 #include "getToken.h"
 
-extern char token_text[20];  //存放单词自身值
+extern char token_text[20];
 extern int cnt_lines;
 extern FILE* fp;
 
 int lexicalAnalyse() {
     int type;
     if (fp == NULL) {
-        printf("文件打开失败\n");
-    } else {
-        printf("文件打开成功\n");
+        printf("fail to open file\n");
+        exit(0);
     }
     printf("\n");
-    printf("  单词类别");
-    printf("\t单词值\n");
+    printf("  WordType");
+    printf("\tWord值\n");
     do {
         type = getToken(fp);
         if (type != ERROR_TOKEN) {
             switch (type) {
                 case IDENT:
-                    printf("   标识符");
+                    printf("%14s", "IDENT");
                     break;
                 case INT_CONST:
-                    printf("  整型常量");
+                    printf("%14s", "INT_CONST");
                     break;
                 case FLOAT_CONST:
-                    printf("浮点型常量");
+                    printf("%14s", "FLOAT_CONST");
                     break;
                 case CHAR_CONST:
-                    printf("  字符常量");
+                    printf("%14s", "CHAR_CONST");
                     break;
                 case STRING_CONST:
-                    printf("字符串常量");
+                    printf("%14s", "STRING_CONST");
                     break;
                 case KEYWORD:
-                    printf("   关键字");
+                    printf("%14s", "KEYWORD");
                     break;
                 case INT:
-                    printf("类型关键字");
+                    printf("%14s", "INT");
                     break;
                 case DOUBLE:
-                    printf("类型关键字");
+                    printf("%14s", "DOUBLE");
                     break;
                 case FLOAT:
-                    printf("类型关键字");
+                    printf("%14s", "FLOAT");
                     break;
                 case CHAR:
-                    printf("类型关键字");
+                    printf("%14s", "CHAR");
                     break;
                 case SHORT:
-                    printf("类型关键字");
+                    printf("%14s", "SHORT");
                     break;
                 case LONG:
-                    printf("类型关键字");
+                    printf("%14s", "LONG");
                     break;
                 case IF:
-                    printf("   关键字");
+                    printf("%14s", "IF");
                     break;
                 case ELSE:
-                    printf("   关键字");
+                    printf("%14s", "ELSE");
                     break;
                 case DO:
-                    printf("   关键字");
+                    printf("%14s", "DO");
                     break;
                 case WHILE:
-                    printf("   关键字");
+                    printf("%14s", "WHILE");
                     break;
                 case FOR:
-                    printf("   关键字");
+                    printf("%14s", "FOR");
                     break;
                 case STRUCT:
-                    printf("   关键字");
+                    printf("%14s", "STRUCT");
                     break;
                 case BREAK:
-                    printf("   关键字");
+                    printf("%14s", "BREAK");
                     break;
                 case SWITCH:
-                    printf("   关键字");
+                    printf("%14s", "SWITCH");
                     break;
                 case CASE:
-                    printf("   关键字");
+                    printf("%14s", "CASE");
                     break;
                 case TYPEDEF:
-                    printf("   关键字");
+                    printf("%14s", "TYPEDEF");
                     break;
                 case RETURN:
-                    printf("   关键字");
+                    printf("%14s", "RETURN");
                     break;
                 case CONTINUE:
-                    printf("   关键字");
+                    printf("%14s", "CONTINUE");
                     break;
                 case VOID:
-                    printf("   关键字");
+                    printf("%14s", "VOID");
                     break;
                 case EQ:
-                    printf("   等于号");
+                    printf("%14s", "EQ");
                     break;
                 case NEQ:
-                    printf("  不等于号");
+                    printf("%14s", "NEQ");
                     break;
                 case ASSIGN:
-                    printf("   赋值号");
+                    printf("%14s", "ASSIGN");
                     break;
                 case LP:
-                    printf("   左括号");
+                    printf("%14s", "LP");
                     break;
                 case RP:
-                    printf("   右括号");
+                    printf("%14s", "RP");
                     break;
                 case LB:
-                    printf("  左大括号");
+                    printf("%14s", "LB");
                     break;
                 case RB:
-                    printf("  右大括号");
+                    printf("%14s", "RB");
                     break;
                 case LM:
-                    printf("  左中括号");
+                    printf("%14s", "LM");
                     break;
                 case RM:
-                    printf("  右中括号");
+                    printf("%14s", "RM");
                     break;
                 case SEMI:
-                    printf("     分号");
+                    printf("%14s", "SEMI");
                     break;
                 case COMMA:
-                    printf("     逗号");
+                    printf("%14s", "COMMA");
                     break;
                 case TIMES:
-                    printf("     乘号");
+                    printf("%14s", "TIMES");
                     break;
                 case DIVIDE:
-                    printf("     除号");
+                    printf("%14s", "DIVIDE");
                     break;
                 case ANNO:
-                    printf("     注释");
+                    printf("%14s", "ANNO");
                     break;
                 case PLUS:
-                    printf("     加法");
+                    printf("%14s", "PLUS");
                     break;
                 case PLUSPLUS:
-                    printf("  自增运算");
+                    printf("%14s", "PLUSPLUS");
                     break;
                 case MINUS:
-                    printf("     减法");
+                    printf("%14s", "MINUS");
                     break;
                 case MINUSMINUS:
-                    printf("  自减运算");
+                    printf("%14s", "MINUSMINUS");
                     break;
                 case MORE:
-                    printf("   大于号");
+                    printf("%14s", "MORE");
                     break;
                 case MOREEQUAL:
-                    printf("大于等于号");
+                    printf("%14s", "MOREEQUAL");
                     break;
                 case LESS:
-                    printf("   小于号");
+                    printf("%14s", "LESS");
                     break;
                 case LESSEQUAL:
-                    printf("小于等于号");
+                    printf("%14s", "LESSEQUAL");
                     break;
                 case INCLUDE:
-                    printf("头文件引用");
+                    printf("%14s", "INCLUDE");
                     break;
                 case MACRO:
-                    printf("   宏定义");
+                    printf("%14s", "MACRO");
                     break;
                 case ARRAY:
-                    printf("     数组");
+                    printf("%14s", "ARRAY");
                     break;
             }
             printf("\t%s\n", token_text);
         } else {
-            printf("\t第%d行出现错误\n", cnt_lines);
+            printf("\tError in line %d\n", cnt_lines);
             break;
         }
     } while (type != -1);
